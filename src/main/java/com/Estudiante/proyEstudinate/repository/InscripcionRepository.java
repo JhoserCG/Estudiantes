0b6b916c7420b6fc4119dccbe4c99b70050ab845
uriolperez <joas.uriol@gmail.com>
2025-01-17 08:08:16 -0500

    agrege repository mas paquetes

---
+++ src/main/java/com/Estudiante/proyEstudinate/repository/InscripcionRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Estudiante.proyEstudinate.repository;
+
+import com.Estudiante.proyEstudinate.model.Inscripcion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author User
+ */
+public interface InscripcionRepository extends JpaRepository<Inscripcion, Long>{
+    
+}
